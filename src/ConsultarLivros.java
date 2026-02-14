fe3916e3a3b1dc57e2ffcd42eb46f291c91a2812
Bruno Leal <brunoleal.tech@hotmail.com>
2026-02-14 13:51:20 -0300

    Solicitando ao usuário os dados de entrada

---
+++ src/ConsultarLivros.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ConsultarLivros {
+    public static void main(String[] args) {
+
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Qual livro você deseja Consultar? ");
+        String livro = ler.nextLine();
+        System.out.println(livro);
+
+
+    }
+}
