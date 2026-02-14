51301764141097fd8301f1128761575a995d60d3
Bruno Leal <brunoleal.tech@hotmail.com>
2026-02-14 16:16:21 -0300

    Iniciando outra atividade. Realizei a entrada de dados solicitado pelo usuario.

---
+++ src/CotacaoAtual.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class CotacaoAtual {
+    public static void main(String[] args) {
+
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Qual Criptomoeda Deseja consultar?");
+        String criptomoeda = ler.nextLine();
+
+
+
+    }
+}
