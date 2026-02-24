97fa204627ae27d06a2c4ae62e90f42c3071b4c1
Bruno Leal <brunoleal.tech@hotmail.com>
2026-02-24 14:04:59 -0300

    Atualizando pastas de Atividades, e iniciando novos exercícios práticos

---
+++ src/br/com/atividades/variaveis/e/tipos/DeclarandoValoresAVariaveis.java
@@ -0,0 +1,16 @@
+package br.com.atividades.variaveis.e.tipos;
+
+public class DeclarandoValoresAVariaveis {
+    public static void main(String[] args) {
+        String nome = "Bruno";
+        int idade = 23;
+        double altura = 1.80;
+        boolean estudante = true;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("É estudante: " + estudante);
+
+    }
+}
